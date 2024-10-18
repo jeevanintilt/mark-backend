41c885554899d34d0bc6bd678931daa032635e3c
jeevanintilt <jeevan.intilt@gmail.com>
2024-10-18 16:41:05 +0530

    added cors middleware

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from routers import user_router
 from database import engine
 import models
@@ -7,6 +8,14 @@ import uvicorn
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # Add your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(user_router.router)
 
